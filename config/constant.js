6d6b3917e8b2bb8073f1676a6b9ace7633bd234e
farman <farman@techcetra.com>
2020-02-24 17:33:49 +0530

    constant removed from gitignore

---
+++ config/constant.js
@@ -0,0 +1,8 @@
+module.exports = {
+      ERR_MSG:"Something Went Wrong ! Please try again later",
+      STATUS_CODE:{
+                success:200,
+                validation:422,
+                error:500
+      }
+}
